e2f72feeeea9b79b30b6f8de15e6bb8b4a5888d0
chanyoung9312-dotcom <chanyoung9312@gmail.com>
2026-09-17 23:17:52 +0900

    1단계: 중복 홈 링크 제거

--- app/analysis/page.js
+++ app/analysis/page.js
@@ -2,7 +2,6 @@ import HomeMdBriefing from "../HomeMdBriefing";
 export default function Page() {
   return (
     <main>
-      <a href="/">← OARS Manager</a>
       <HomeMdBriefing />
     </main>
   );
